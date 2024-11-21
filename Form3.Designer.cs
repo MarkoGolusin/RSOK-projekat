829576c1a23970cb09ed317bba29a33f3a57523a
Jerkov Ivan <ivanjerkov2002@gmail.com>
2024-11-21 16:12:03 +0100

    Izmena fajlova

---
+++ Form3.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace SeminarskiRSOK
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form3));
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(-5, -4);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(811, 461);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Calibri", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(313, 20);
+            label1.Name = "label1";
+            label1.Size = new Size(178, 33);
+            label1.TabIndex = 1;
+            label1.Text = "ADMIN OPCIJE";
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(128, 255, 255);
+            button1.Font = new Font("Calibri", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(690, 20);
+            button1.Name = "button1";
+            button1.Size = new Size(98, 33);
+            button1.TabIndex = 2;
+            button1.Text = "POČETNA";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.FromArgb(255, 192, 128);
+            button2.Font = new Font("Calibri", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.Location = new Point(326, 125);
+            button2.Name = "button2";
+            button2.Size = new Size(152, 33);
+            button2.TabIndex = 3;
+            button2.Text = "PREGLED KARATA";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.FromArgb(255, 192, 128);
+            button3.Font = new Font("Calibri", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button3.Location = new Point(326, 183);
+            button3.Name = "button3";
+            button3.Size = new Size(152, 33);
+            button3.TabIndex = 4;
+            button3.Text = "UNOS KARATA";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.FromArgb(255, 192, 128);
+            button4.Font = new Font("Calibri", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button4.Location = new Point(326, 237);
+            button4.Name = "button4";
+            button4.Size = new Size(152, 33);
+            button4.TabIndex = 5;
+            button4.Text = "IZMENA KARATA";
+            button4.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            button5.BackColor = Color.FromArgb(255, 192, 128);
+            button5.Font = new Font("Calibri", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button5.Location = new Point(326, 294);
+            button5.Name = "button5";
+            button5.Size = new Size(152, 33);
+            button5.TabIndex = 6;
+            button5.Text = "BRISANJE KARAZA";
+            button5.UseVisualStyleBackColor = false;
+            // 
+            // Form3
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Controls.Add(pictureBox1);
+            Name = "Form3";
+            Text = "Form3";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        public Label label1;
+    }
+}
---
+++ Form4.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace SeminarskiRSOK
+{
+    partial class Form4
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(255, 192, 128);
+            button1.Font = new Font("Calibri", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(184, 538);
+            button1.Name = "button1";
+            button1.Size = new Size(152, 44);
+            button1.TabIndex = 0;
+            button1.Text = "REZERVACIJA";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // Form4
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(136, 204, 227);
+            ClientSize = new Size(531, 621);
+            Controls.Add(button1);
+            Name = "Form4";
+            Text = "Form4";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+    }
+}
