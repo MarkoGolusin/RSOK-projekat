d0a1591696dcc2cf0676d4971bb2588de010ca84
MarkoGolusin <133912557+MarkoGolusin@users.noreply.github.com>
2024-12-03 19:37:20 +0100

    Azuriran Form5.cs
    Dodat DataGrid i spojen sa bazom podataka

--- RSOK-projekat-master/Form5.cs
+++ RSOK-projekat-master/Form5.cs
@@ -15,6 +15,53 @@ namespace SeminarskiRSOK
         public Form5()
         {
             InitializeComponent();
+            BinGrid();
+        }
+
+        private void binDataGrid()
+        {
+            string constring = "Data Source=DESKTOP-1VNGR39;Initial Catalog=AvioKarte;Integrated Security=True;Trust Server Certificate=True";
+            using (SqlConnection con = new SqlConnection(constring))
+            {
+
+
+
+
+            }
+
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.BinGrid();
+        }
+
+        private void BinGrid()
+        {
+            string constring = @"Data Source=DESKTOP-1VNGR39;Initial Catalog=AvioKarte;Integrated Security=True;";
+            using (SqlConnection con = new SqlConnection(constring))
+            {
+                using (SqlCommand cmd = new SqlCommand("Select * FROM karta", con))
+                {
+
+                    using (SqlDataAdapter sda = new SqlDataAdapter(cmd))
+                    {
+                        using (DataTable dt = new DataTable())
+                        {
+                            sda.Fill(dt);
+                            dataGridView1.DataSource = dt;
+                        }
+                    }
+
+                }
+
+
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
         }
     }
 }
