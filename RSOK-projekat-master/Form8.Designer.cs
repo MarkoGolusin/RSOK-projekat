35d8c0e7db12646d3fb886d0a351bcebbff611e4
Jerkov Ivan <164182383+JerkovIvan@users.noreply.github.com>
2024-12-04 20:19:28 +0100

    Dodadavanje formova
    Dodato je form 7 (Pregled karata za admina), form 8 (Unos karata) i form 9 (Izmena karata).

---
+++ RSOK-projekat-master/Form8.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace SeminarskiRSOK
+{
+    partial class Form8
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dataGridView1 = new DataGridView();
+            button2 = new Button();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(22, 63);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(1163, 381);
+            dataGridView1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.FromArgb(128, 255, 255);
+            button2.Font = new Font("Calibri", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.Location = new Point(1096, 12);
+            button2.Name = "button2";
+            button2.Size = new Size(89, 30);
+            button2.TabIndex = 3;
+            button2.Text = "NAZAD";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(255, 192, 128);
+            button1.Font = new Font("Calibri", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.ForeColor = SystemColors.ControlText;
+            button1.Location = new Point(525, 518);
+            button1.Name = "button1";
+            button1.Size = new Size(153, 44);
+            button1.TabIndex = 4;
+            button1.Text = "UNESI";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // Form8
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(136, 204, 227);
+            ClientSize = new Size(1207, 621);
+            Controls.Add(button1);
+            Controls.Add(button2);
+            Controls.Add(dataGridView1);
+            Name = "Form8";
+            Text = "Form8";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private Button button2;
+        private Button button1;
+    }
+}
