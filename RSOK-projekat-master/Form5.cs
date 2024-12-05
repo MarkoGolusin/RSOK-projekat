77a40fea66bb3d26e1af1bc1b54a61f134d1697a
MarkoGolusin <133912557+MarkoGolusin@users.noreply.github.com>
2024-12-05 15:27:16 +0100

    Dodato brisanje karata
    Dodato brisanje karata klikom na kartu u tabeli i zatim klikom na dugme obisi

--- RSOK-projekat-master/Form5.cs
+++ RSOK-projekat-master/Form5.cs
@@ -70,5 +70,88 @@ namespace SeminarskiRSOK
      this.Hide();
 
  }
+
+private void button1_Click(object sender, EventArgs e)
+{
+    try
+    {
+       
+        if (dataGridView1.CurrentRow != null)
+        {
+            int idKarte = (int)dataGridView1.CurrentRow.Cells[0].Value;
+            string constring = @"Data Source=DESKTOP-1VNGR39;Initial Catalog=AvioKarte;Integrated Security=True;";
+
+            using (SqlConnection con = new SqlConnection(constring))
+            {
+                
+                con.Open();
+
+                using (SqlCommand cmd = new SqlCommand("DELETE FROM karta WHERE idKarte=@idKarte", con))
+                {
+                    cmd.Parameters.AddWithValue("@idKarte", idKarte);
+
+                    
+                    int rowsAffected = cmd.ExecuteNonQuery();
+
+                    if (rowsAffected > 0)
+                    {
+                        MessageBox.Show("Obrisano");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Nije pronadjen id za brisanje");
+                    }
+                }
+            }
+
+            // Refresh the DataGridView to reflect the deletion
+            ResetTabele();
+        }
+        else
+        {
+            MessageBox.Show("Izaberite red za brisanje");
+        }
+    }
+    catch (Exception ex)
+    {
+        
+        MessageBox.Show("Greska: " + ex.Message);
+    }
+}
+
+private void ResetTabele()
+{
+    try
+    {
+        string constring = @"Data Source=DESKTOP-1VNGR39;Initial Catalog=AvioKarte;Integrated Security=True;";
+        using (SqlConnection con = new SqlConnection(constring))
+        {
+            
+            con.Open();
+
+            
+            string selectQuery = "SELECT * FROM karta"; 
+            using (SqlCommand cmd = new SqlCommand(selectQuery, con))
+            {
+                
+                DataTable dt = new DataTable();
+                using (SqlDataAdapter da = new SqlDataAdapter(cmd))
+                {
+                   
+                    da.Fill(dt);
+                }
+
+                
+                dataGridView1.DataSource = dt;
+            }
+        }
+    }
+    catch (Exception ex)
+    {
+        
+        MessageBox.Show("Greska pri prikazivanju nove tabele " + ex.Message);
+    }
+}
+ 
     }
 }
